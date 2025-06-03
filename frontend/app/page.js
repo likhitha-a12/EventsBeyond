dcc93c176c51b729ae0755a9bf612e112b4da3d9
likhitha-a12 <likitha.rebba@gmail.com>
2025-06-03 19:07:10 +0530

    Re-added frontend and backend folders after removing submodules

---
+++ frontend/app/page.js
@@ -0,0 +1,11 @@
+// app/page.js
+'use client';
+import Carousel from './components/Carousel';
+
+export default function HomePage() {
+  return (
+    <main>
+      <Carousel />
+    </main>
+  );
+}
